dc6ee8e22ebe494a150314fee69f734bf7fb9b62
Frans Skarman <frans.skarman@gmail.com>
2016-09-14 22:47:00 +0200

    Add missing vec module

---
+++ src/Vec.elm
@@ -0,0 +1,17 @@
+module Vec exposing (Size, Position)
+
+
+type alias Size =
+    {
+        width: Float,
+        height: Float
+    }
+
+type alias Position =
+    {
+        x: Float,
+        y: Float
+    }
+
+
+
